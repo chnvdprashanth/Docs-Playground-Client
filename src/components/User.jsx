05e38709c5303ef6560514f693e3b5318b5c8ede
chnvdprashanth <chnvdprashanth@gmail.com>
2024-09-05 21:32:26 +0530

    Updating User.jsx UI

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -13,10 +13,10 @@ const User = () => {
         src={user?.image}
         alt="user-profile"
         className="relative rounded-full cursor-pointer"
-        onClick={() => setToggleUserOptions(true)}
+        onClick={() => setToggleUserOptions(!toggleUserOptions)}
       />
       {toggleUserOptions && (
-        <div className="bg-zinc-900/90 text-white absolute right-0 top-14 w-40 h-auto rounded-lg border-[1px] border-zinc-100 p-2 flex flex-col items-center gap-y-2">
+        <div className="bg-zinc-900/50 text-white absolute right-0 top-14 w-40 h-auto rounded-lg shadow-lg p-2 flex flex-col items-center gap-y-4">
           <p className="text-center w-full text-lg">{user?.name}</p>
           <button className="outline-none bg-red-600 px-4 py-2 rounded-lg">
             Sign Out.
