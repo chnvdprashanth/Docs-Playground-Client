10224cb5dc2f58c29966e36c25a9f6e906018618
chnvdprashanth <chnvdprashanth@gmail.com>
2024-09-05 19:47:23 +0530

    Update User.jsx after using Cloudinary in backend.

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,36 +1,13 @@
-import { useEffect, useState } from "react";
 import { useGetUser } from "../contexts/User";
 
 const User = () => {
   const { user } = useGetUser();
-  const [imgURL, setImgURL] = useState("");
-
-  useEffect(() => {
-    const handleUserImage = async () => {
-      try {
-        const res = await fetch(
-          `https://docs-playground.onrender.com/${user?.image}`,{
-            method: "GET",
-            credentials: "include"
-          }
-        );
-        const imgBlob = await res.blob();
-  
-        setImgURL(URL.createObjectURL(imgBlob));
-      } catch (err) {
-        console.error("Error in Fetching Profile Image: ", err);
-      }
-    };
-    if(user?.image){
-      handleUserImage();
-    }
-  },[user]);
 
   if (user === null) return;
 
   return (
     <div>
-      <img src={imgURL} alt="user-profile" className="rounded-full" />
+      <img src={user?.image} alt="user-profile" className="rounded-full" />
     </div>
   );
 };
