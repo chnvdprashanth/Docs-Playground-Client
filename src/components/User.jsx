5e408ac16e5548392dab354848030b98c3bc0775
chnvdprashanth <chnvdprashanth@gmail.com>
2024-09-05 21:01:36 +0530

    Updating User.jsx component.

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import { useGetUser } from "../contexts/User";
 
 const User = () => {
   const { user } = useGetUser();
+  const [toggleUserOptions,setToggleUserOptions] = useState(false);
 
   if (user === null) return;
 
   return (
     <div>
-      <img src={user?.image} alt="user-profile" className="rounded-full" />
+      <img src={user?.image} alt="user-profile" className="relative rounded-full" onClick={()=>setToggleUserOptions(true)} />
+      {
+        toggleUserOptions && (
+          <div className="bg-zinc-800 opacity-25 absolute w-auto h-auto p-2 flex flex-col items-center gap-y-2">
+            <li>{user?.name}</li>
+            <li>Sign Out</li>
+          </div>
+        )
+      }
     </div>
   );
 };
