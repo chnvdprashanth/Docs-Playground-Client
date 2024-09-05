bf84b0d627397bc37deedeb4ab71684fbf8815bf
chnvdprashanth <chnvdprashanth@gmail.com>
2024-09-05 16:13:23 +0530

    User.jsx updated to render user-profile-img.

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,32 @@
+import { useEffect } from "react";
 import { useGetUser } from "../contexts/User";
 
 const User = () => {
   const { user } = useGetUser();
+  const imgURL = "";
+
+  const handleUserImage = async () => {
+    try {
+      const res = await fetch(
+        `https://docs-playground.onrender.com/${user?.image}`
+      );
+      const imgBlob = res.blob();
+
+      imgURL = URL.createObjectURL(imgBlob);
+    } catch (err) {
+      console.error("Error in Fetching Profile Image: ",err);
+    }
+  };
+  useEffect(() => {
+    handleUserImage();
+  });
+
   if (user === null) return;
+
   return (
     <div>
       <img
-        src={`https://docs-playground.onrender.com/${user?.image}`}
+        src={imgURL}
         alt="user-profile"
         className="rounded-full"
       />
