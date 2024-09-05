eab519f591b38eba844262ac2638d16f6895a18c
chnvdprashanth <chnvdprashanth@gmail.com>
2024-09-05 16:30:49 +0530

    bug fix in User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from "react";
 import { useGetUser } from "../contexts/User";
 
 const User = () => {
   const { user } = useGetUser();
+  const [imgURL, setImgURL] = useState("");
+
+  useEffect(() => {
+    const handleUserImage = async () => {
+      try {
+        const res = await fetch(
+          `https://docs-playground.onrender.com/${user?.image}`,{
+            method: "GET",
+            credentials: "include"
+          }
+        );
+        const imgBlob = res.blob();
+  
+        setImgURL(URL.createObjectURL(imgBlob));
+      } catch (err) {
+        console.error("Error in Fetching Profile Image: ", err);
+      }
+    };
+    if(user?.image){
+      handleUserImage();
+    }
+  },[user]);
 
   if (user === null) return;
 
   return (
     <div>
-      <img src={`https://docs-playground.onrender.com/${user?.image}`} alt="user-profile" className="rounded-full" />
+      <img src={imgURL} alt="user-profile" className="rounded-full" />
     </div>
   );
 };
